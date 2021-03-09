5744c175d77d922a29ad0feb85481c4e752d071c
veronikadumalo <dumalo44084@gmail.com>
2021-03-09 19:50:51 +0100

    started create detail page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { ProductConsumer } from '../context';
+import { Link } from 'react-router-dom';
+
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h1>Details</h1>
-            </div>
+            <ProductConsumer>
+                {value => {
+                    const { id, title, price, img, moreImg, info, inCart, size, selectedSize } = value.detailProduct;
+                    return (
+                        <div className="container py-3">
+                        <div className="row">
+                            <div className="col-10 mx-auto text-center my-5 text-capitalize">
+                                <h1>{title}</h1>
+                            </div>
+                        </div>
+                        <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    
+                                </div>
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    
+                                </div>
+                        </div>
+                    </div>
+                        );
+                }}
+            </ProductConsumer>
         );
     }
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ export default function Title({ title }) {
     return (
         <div className="row">
             <div className="col-10 mx-auto my-5 text-center text-title">
-                <h1 className="text-capitalize ">{title}</h1>
+                <h1 className="text-capitalize">{title}</h1>
             </div>
         </div>
         );
