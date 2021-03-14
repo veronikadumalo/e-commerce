db9f70c27bb6a8b2a4c01a9eb2eae6a44216d638
veronikadumalo <dumalo44084@gmail.com>
2021-03-14 16:02:49 +0100

    create a function for showing added to cart products

---
+++ src/components/CartProduct.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function CartProduct({ item, value }) {
+    const { id, img, title, proce, selectedSize, count, total } = item;
+    return (
+        <div></div>
+        );
+}
