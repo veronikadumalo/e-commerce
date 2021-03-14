508eb166b72699baf58f4982d423e03bcec45c7a
veronikadumalo <dumalo44084@gmail.com>
2021-03-14 14:12:55 +0100

    first commit 14/03/21

--- src/context.js
+++ src/context.js
@@ -7,8 +7,10 @@ class ProductProvider extends Component {
     state = {
         products: storeProducts,
         detailProduct: detailProduct,
+        cart: [],
         smallDetail: false,
-        smallDetailProduct: detailProduct
+        smallDetailProduct: detailProduct,
+        
     };
     getItem = (id) => {
         const product = this.state.products.find(item => item.id === id);
@@ -19,10 +21,20 @@ class ProductProvider extends Component {
         this.setState(() => {
             return { detailProduct:product}
         })
-        console.log( detailProduct, product);
+        console.log( detailProduct, product); 
     };
     addToCart = (id) => {
-        console.log("add to cart");
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+        this.setState(() => {
+            return { products: tempProducts, cart: [...this.state.cart, product] };
+        })
+        
     };
     sSize = (id) => {
         const product = this.getItem(id);
