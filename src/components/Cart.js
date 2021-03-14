d097a7e8e84a3def7dda0fff356353a37e1fe6a5
veronikadumalo <dumalo44084@gmail.com>
2021-03-14 15:31:00 +0100

    create different title in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import { ProductConsumer } from '../context';
+
 
 export default class Cart extends Component {
     render() {
         return (
-            <React.Fragment>
-                <Title title="my cart" />
-            </React.Fragment> 
+            <ProductConsumer>
+                {value => {
+                    const { cart } = value;
+                    console.log(cart);
+                    if (cart.length > 0) {
+                        return (
+                            <React.Fragment>
+                                <Title title="my cart" />
+                            </React.Fragment>
+                        );
+                    }
+                    else {
+                        return (
+                            <React.Fragment>
+                                <Title title="your cart is currently empty" />
+                            </React.Fragment>
+                        );
+                    }
+                }}
+            </ProductConsumer> 
         );
     }
 }
