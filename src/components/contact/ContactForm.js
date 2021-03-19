956cb5b98d2d9581c7c9a5be8d5a39f7df208de5
veronikadumalo <dumalo44084@gmail.com>
2021-03-19 20:05:57 +0100

    make contact form functional using useForm from react-hook-form

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,7 +1,7 @@
 import {useForm} from 'react-hook-form';
 
 const ContactFrom =()=>{
-    const {register, error, handleSubmit, reset} = useForm();
+    const {register, errors, handleSubmit, reset} = useForm();
     const onSubmit =async(data) => {
         console.log('name', data.name);
         console.log('email', data.email);
@@ -12,18 +12,53 @@ const ContactFrom =()=>{
                     <div className="container">
                         <div className="row">
                             <div className="col-8 mx-auto text-capitalize">
-                                <form>
+                                <form id="contact-form" onSubmit={handleSubmit(onSubmit)} noValidate>
                                     <div className="form-group">
                                         <label for="name-input">name:</label>
-                                        <input type="text" id="name-input" className="form-control"></input>
+                                        <input type="text" 
+                                        name="name"
+                                        ref={register({
+                                            required: {value:true, message:"Please enter your name"},
+                                            maxLength:{
+                                                value:30,
+                                                message:"Please use 30 charter or less"
+                                            }
+                                        })}
+                                        id="name-input" className="form-control formInput"></input>
+                                        {errors.name &&
+                                        <span className="errorMessage">
+                                            {errors.name.message}
+                                        </span>}
                                     </div>
                                     <div className="form-group">
                                         <label for="email-input">email:</label>
-                                        <input type="email" id="email-input" className="form-control"></input>
+                                        <input type="email" 
+                                        name="email"
+                                        ref={register({
+                                            required: true,
+                                            pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                                        })}
+                                        id="email-input" className="form-control formInput"></input>
+                                        {errors.email &&(
+                                            <span className="errorMessage">
+                                                Please enter a valid email address
+                                            </span>
+                                        )}
                                     </div>
                                     <div className="form-group">
                                         <label for="message">message:</label>
-                                        <textarea id="message" className="form-control"></textarea>
+                                        <textarea
+                                        name="message"
+                                        ref={register({
+                                            required:true
+                                        })}
+                                        rows={3}
+                                        id="message" className="form-control formInput"></textarea>
+                                        {errors.message && (
+                                            <span className="errorMessage">
+                                                Please enter a message
+                                            </span>
+                                        )}
                                     </div>
                                     <button type="submit" className="detail-button">send</button>
                                 </form>
