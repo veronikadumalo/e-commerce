cb81eda72bbba3f9cd37dca1fa1c5e2ec939c28e
veronikadumalo <dumalo44084@gmail.com>
2021-03-25 19:17:49 +0100

    update contact info style

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import Raect from 'react';
 export default function Title({ title }) {
     return (
         <div className="row">
-            <div className="col-10 mx-auto mt-5 text-center text-title mb-1">
+            <div className="col-10 mx-auto mt-5 text-center text-title ">
                 <h1 className="text-capitalize">{title}</h1>
             </div>
         </div>
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -9,13 +9,13 @@ export default class Contact extends Component {
         return (
             <React.Fragment>
                 <Title title="contact us" />
-                <div className="container mb-5 mt-1">
+                <div className="container  mt-1">
                     <h4 className="text-center">Got a quation? We`d <i className="far fa-heart"></i> to help You!</h4>
                     <div className="row mx-auto">
                         <div className="col-10 col-lg-7 mx-auto">
                            <ContactFrom /> 
                         </div>
-                        <div className="col-10  col-lg-4 ml-lg-5 mt-5 mx-auto">
+                        <div className="col-12 col-lg-4 mt-5 mx-auto">
                            <StoreInformation /> 
                         </div>
                     </div>
